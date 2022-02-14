35881a15bd1ebe4a375260779afe81ac92093114
cs21s006 <cs21s006@smail.iitm.ac.in>
2022-02-14 10:53:03 +0530

    added imports

--- neural_networks/__init__.py
+++ neural_networks/__init__.py
@@ -1,4 +1,5 @@
 from .activations import activations
 from .layers import Layer
-from .layers import Layer
 from .neural_network import NeuralNetwork
+from .optimizer import Optimizer, clip_gradients
+from .losses import losses
